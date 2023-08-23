be4f8fd031d30aa944a9ecb3d1870642cb701a88
Sharad Varshney <Sharad.varshney@publicisgroupe.com>
2023-08-23 12:54:57 -0700

    changed file name

---
+++ evaluate/evaluate.py
@@ -0,0 +1,16 @@
+import os
+
+
+def load_model(path):
+    return ""
+
+
+class Evaluate:
+    def __init__(self, model_path):
+        self.model = load_model(model_path)
+
+    def evaluate(self):
+        """
+
+        :return:
+        """
